0b92f6a284aeb77f63676399c8a50e8bfc007418
Kristablackey <blackey@hartford.edu>
2026-02-17 16:03:39 -0500

    initials

--- program-1/Assignment1Program1.java
+++ program-1/Assignment1Program1.java
@@ -1,5 +1,37 @@
+import java.util.Scanner;
+
 public class Assignment1Program1 {
   public static void main(String[] args) {
 
+  //*************************************
+  // Print fist and last initials in large block letters
+  //*************************************
+    {
+      System.out.println("K    K   BBBBB");
+      System.out.println("K   K    B    B");
+      System.out.println("K  K     B   B");
+      System.out.println("KKK      BBBBB");
+      System.out.println("K. K     B   B");
+      System.out.println("K   K    B    B");
+      System.out.println("K    K   BBBBB");
+    }
+  //*************************************
+  // Enter a 5-Character string
+  //*************************************
+    {
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Enter a 5-character string:");
+    String str = input.nextLine();
+
+    if (str.length()== 5) {
+    String trimmedRevered =
+    new StringBuilder(str.substring(1,4)) .reverse().
+    toString();
+
+    System.out.println("Step 1 result"+ trimmedRevered);}
+
+    input.close();
+    }
   }
 }
--- program-2/Assignment1Program2.java
+++ program-2/Assignment1Program2.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class Assignment1Program2 {
   public static void main(String[] args) {
 
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Base Conversion Program");
+
+    System.out.print("Enter a base (2-9): ");
+    int base = input.nextInt();
+
+    int maxNumber = (int) Math.pow(base, 4) - 1;
+    System.out.println("The maximun 4-digit, base 10 number in base " + base + " is: " + maxNumber + ".");
+
+    System.out.print("Now enter a base 10 number in base" + maxNumber +  ".");
+    int number = input.nextInt();
+
+    int d1 = number / (base * base * base);
+    int remainder1 = number % (base * base * base);
+
+    int d2 = remainder1 / (base * base);
+    int remainder2 = remainder1 % (base * base);
+
+    int d3 = remainder2 / base;
+    int d4 = remainder2 % base;
+
+    System.out.println("The base " + base + " representation of " + number + " is: " + d1 + d2 + d3 + d4);
+
+    input.close();
   }
+
 }
