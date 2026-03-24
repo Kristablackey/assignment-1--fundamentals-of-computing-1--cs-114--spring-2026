73143405ed039be482d425824b903e9679287143
Kristablackey <blackey@hartford.edu>
2026-03-24 15:43:58 -0400

    Update Assignment1Program2.java

--- program-2/Assignment1Program2.java
+++ program-2/Assignment1Program2.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 public class Assignment1Program2 {
   public static void main(String[] args) {
 
+    //*************************************
+    // Base Conversion Program
+    //*************************************
     Scanner input = new Scanner(System.in);
 
     System.out.print("Base Conversion Program");
